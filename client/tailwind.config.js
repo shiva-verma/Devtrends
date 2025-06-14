44f256b484b20824bcfd9f361d15e004174265cb
shiva-verma <jztshiva@gmail.com>
2025-06-14 16:06:42 +0530

    fixing css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,8 +3,6 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
